a4bdf29cfc2931ec5a24ced595e70207e2ec0825
Brian Sheldon <briansouthpas@gmail.com>
2024-08-20 14:45:07 -0700

    changing mainplayer args to be object{}

--- objects/main-player.js
+++ objects/main-player.js
@@ -1,47 +1,66 @@
 export class MainPlayer {
-    constructor(color) {
-        this.color = color;
-        this.gravity = 0.1;
-        this.radius = 10;
-        this.loc = {
-            x: 50,
-            y: 50
-        };
+    constructor({img, gravity, radius, loc, friction, canvasWidth, canvasHeight, 
+                jumpSpeed, leftRightSpeed}) {
+        this.img = img;
+        this.gravity = gravity;
+        this.radius = radius;
+        this.imgSize = 1.3*Math.sqrt(2)*radius;
+        this.loc = loc;
         this.vel = {
             x: 0,
-            y: 0
+            y: 0, 
+            theta: 0
         };
-        this.friction = 0.99;
-    }
-    draw(c, windowWidth, windowHeight) {
+        this.friction = friction;
+        this.theta = 0;
+        this.pointing_right = 1;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.jumpSpeed = jumpSpeed;
+        this.leftRightSpeed = leftRightSpeed;
 
-        this.update(windowWidth, windowHeight);
-        c.fillStyle = this.color;
-        c.beginPath();
-        c.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI * 2, false);
-        c.fill();
+        // flag for halting controls
+        this.halted = false;
     }
-    update(windowWidth, windowHeight) {
+    draw(c) {
 
-        // apply gravity
+        this.update();
+        c.save();
+        
+        c.translate(this.loc.x, this.loc.y);
+        c.rotate(-this.pointing_right * this.theta);
+        c.scale(this.pointing_right, 1);
+        c.translate(-this.loc.x, -this.loc.y);
+        c.drawImage(this.img, this.loc.x - this.radius, this.loc.y - this.radius, 1.2*this.imgSize, this.imgSize);
+
+        c.restore();
+    }
+    update() {
+
+
+        // apply gravity and friction to vel
         this.vel.y += this.gravity;
+        this.vel.theta -= 0.002*this.gravity; 
         this.vel.x *= this.friction;
         this.vel.y *= this.friction;
 
 
         this.loc.x += this.vel.x;
         this.loc.y += this.vel.y;
+        this.theta += this.vel.theta;
+        if (this.theta < -Math.PI/2) {this.theta = -Math.PI/2};
+        if (this.theta > Math.PI/3) {this.theta = Math.PI/3; this.vel.theta = 0};
 
 
         // apply bounds
-        if (this.loc.x + this.radius > windowWidth) {
-            this.loc.x = windowWidth - this.radius;
+        if (this.loc.x + this.radius > this.canvasWidth) {
+            this.loc.x = this.canvasWidth - this.radius;
         }
         if (this.loc.x - this.radius < 0) {
             this.loc.x = this.radius;
         }
-        if (this.loc.y + this.radius > windowHeight) {
-            this.loc.y = windowHeight - this.radius;
+        if (this.loc.y + this.radius > this.canvasHeight) {
+            this.loc.y = this.canvasHeight - this.radius;
         }
         if (this.loc.y - this.radius < 0) {
             this.loc.y = this.radius;
@@ -51,14 +70,23 @@ export class MainPlayer {
         doc.addEventListener('keydown', this.keyDown.bind(this));
     } 
     keyDown(e) {
+
+        // ignore inputs when player is halted
+        if (this.halted) {
+            return;
+        }
+
         if (e.code == 'Space') {
-            this.vel.y = -7;
+            this.vel.y = -this.jumpSpeed;
+            this.vel.theta = 1;
         }
         if (e.code == 'KeyD' || e.code == 'ArrowRight') {
-            this.vel.x = 6;
+            this.vel.x = this.leftRightSpeed;
+            this.pointing_right = 1;
         }
         if (e.code == 'KeyA' || e.code == 'ArrowLeft') {
-            this.vel.x = -6;
+            this.vel.x = -this.leftRightSpeed;
+            this.pointing_right = -1;
         }
     }
 }

33a0f32e2015d7ef571623101fb82a0455d13559
Brian Sheldon <briansouthpas@gmail.com>
2024-08-20 14:43:41 -0700

    moving various functions into util file

---
+++ util.js
@@ -0,0 +1,48 @@
+
+export function handleGameOver(mainPlayer, rungsArray, spawnInterval) {
+
+    // prevent more rungs from being created
+    clearInterval(spawnInterval);
+
+    // stop rungs
+    rungsArray.forEach(rung => {
+        rung.vel = {x: 0, y: 0};
+    })
+
+    // stop player
+    mainPlayer.halted = true;
+}
+
+
+export function drawCount(c, g) {
+
+    // apply g.countConfig to context
+    Object.keys(g.countConfig).forEach(key => {
+        c[key] = g.countConfig[key];
+    });
+    c.strokeText(g.count, g.countConfig.loc.x, g.countConfig.loc.y);
+    c.fillText(g.count, g.countConfig.loc.x, g.countConfig.loc.y);
+}
+
+
+export function checkCollision_and_pass(mainPlayer, rungsArray, g) {
+    let x = mainPlayer.loc.x;
+    let y = mainPlayer.loc.y;
+    let radius = mainPlayer.radius;
+
+    // first check collision with ground
+    if (y + radius >= g.canvasHeight) {
+        return true;
+    }
+
+    // check collision with each rung
+    for (let i=0; i<rungsArray.length; i++) {
+        let rung = rungsArray[i];
+        let [collision, passed] = rung.checkCollision_and_pass(x, y, radius);
+        if (collision) {
+            return true;
+        }
+        g.count += passed;
+    }
+    return false;
+}

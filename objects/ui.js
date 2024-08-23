fea8985c2bb55937d68a4722f85fe0de505eb067
Brian Sheldon <briansouthpas@gmail.com>
2024-08-23 16:02:32 -0700

    finalizing creation of buttons, and ui

--- objects/ui.js
+++ objects/ui.js
@@ -1,33 +1,26 @@
-export default class UI {
-    constructor({loc, width, height, backgroundColor}) {
+export class UIFrame {
+    constructor({loc}) {
 
-        // array of buttons within UI
-        this.buttonsArray = [];
+        // array of elements within UI
+        this.elements = [];
 
         this.loc = loc;
-        this.width = width;
-        this.height = height;
-        this.backgroundColor = backgroundColor;
 
 
         // flag for seeing ui
-        // if not visible cannot interact with buttons
+        // if not visible cannot interact with elements
         this.visible = false;
     }
 
     draw(c) {
+
+        // if not visible return early
         if (!this.visible) {return;}
 
-        // draw background
-        if (this.backgroundColor!=undefined) {
-            c.fillStyle = this.backgroundColor;
-            c.fillRect(this.loc.x, this.loc.y, this.width, this.height);
-        }
-
-        // draw buttons
-        this.buttonsArray.forEach(button => 
-                                    {button.draw(c);}
-                                );
+        // draw elements
+        this.elements.forEach(element => 
+                                 {element.draw(c);}
+                              );  
 
     }
 
@@ -36,76 +29,182 @@ export default class UI {
         // if not visible return early
         if (!this.visible) {return;}
 
-        // if click not within ui return early
-        if (x < this.loc.x || x > this.loc.x + this.width || 
-            y < this.loc.y || y > this.loc.y + this.height) {
-            return;
-        }
-
-        // check each button to see if within click
+        // check each element to see if within click
         let x_rel = x - this.loc.x;
         let y_rel = y - this.loc.y;
-        for (let i=0; i<this.buttonsArray.length; i++) {
-            let button = this.buttonsArray[i];
-            if (x_rel > button.loc.x && x_rel < button.loc.x + button.width && 
-                y_rel > button.loc.y && y_rel < button.loc.y + button.height) {
-                button.onClick();
-                return; // only one button can be clicked
+        for (let i=0; i<this.elements.length; i++) {
+            let element = this.elements[i];
+            if (x_rel > element.loc.x && x_rel < element.loc.x + element.width && 
+                y_rel > element.loc.y && y_rel < element.loc.y + element.height) {
+                element.onClick();a
+                return; // only one element can be clicked
             }
         }
     }
-    addButton(button) {
-        // check if button fits
-        if (button.loc.x<0 || button.loc.y<0 || 
-            button.loc.x+button.width>this.width || button.loc.y+button.height>this.height) {
-        if (button.loc.x + button.width > this.width || button.loc.y + button.height > this.height) {
-            throw new Error('Button does not fit within UI');
-        }
-    }
+    addElement(element) {
+        // add element to elements array
+        this.elements.push(element);
+        element.uiParent = this;
+        console.log(this.loc);
     }
 }
 
 
-class button {
-    constructor({uiParent, doc, loc, width, height, font, fillStyle, textAlign, strokeStyle, lineWidth, onClick}) {
+export class Button {
+    constructor({uiParent, loc, width, height, unClickedStyle, clickedStyle, onClick}) {
+        /*  
+            style: {
+                text (str): string of text to display on button
+                font: font style for text
+                fillStyle: fill style for text
+                strokeStyle: stroke style for text
+                lineWidth: line-width for text
+                textAlign: text alignment
+                textBaseline: text baseline
+                bkgColor: background color of button
+                bkgBorderColor: border color of button
+                bkgBorderWidth: border width of button
+            }
+        */
         this.uiParent = uiParent;
         this.loc = loc; // rel to uiParent
         this.width = width;
         this.height = height;
-        this.fontArgs = {
-            font: font,
-            fillStyle: fillStyle,
-            textAlign: textAlign,
-            strokeStyle: strokeStyle,
-            lineWidth: lineWidth
-        };
-        this.text = text;
-        this.font = font;
-        this.fillStyle = fillStyle;
-        this.strokeStyle = strokeStyle;
-        this.lineWidth = lineWidth;
         this.onClick = onClick;
+        this.text = 'Button';
+        this.background = 'white';
+
+        // styles
+        this.unClickedStyle = unClickedStyle;
+        this.clickedStyle = clickedStyle;
+        this.clicked = false;
+
+        // initialize onClick with additional style change added
+        this.init_onClick(onClick);
+    }
+    init_onClick(onClick) {
+        this.onClick = () => {
+            this.clicked = !this.clicked;
+            onClick();
+        }
     }
     draw(c) {
         c.save();
 
-        // apply uiParent loc
-        c.translate(this.uiParent.loc.x, this.uiParent.loc.y);
+        // translate to uiParent loc if exists
+        if (this.uiParent !== undefined) {
+            c.translate(this.uiParent.loc.x, this.uiParent.loc.y);
+        }
 
-        // draw button
-        c.fillStyle = this.fillStyle;
-        c.strokeStyle = this.strokeStyle;
-        c.lineWidth = this.lineWidth;
+        // draw button background
+        this.applyBkgStyle(c);
         c.fillRect(this.loc.x, this.loc.y, this.width, this.height);
         c.strokeRect(this.loc.x, this.loc.y, this.width, this.height);
 
-        // draw text
-        c.font = this.font;
+        // draw button text
+        this.applyTextStyle(c);
+        c.fillText(this.text, this.loc.x + this.width/2, this.loc.y + this.height/2);
+
+        c.restore();
+    }
+    applyTextStyle(c) {
+
+        // set style based on clicked
+        let style = this.clicked ? this.clickedStyle : this.unClickedStyle;
+
+        // apply style for text
+        this.text = style.text;
+        c.font = style.font;
+        c.fillStyle = style.fillStyle;
+        c.strokeStyle = style.strokeStyle;
+        c.lineWidth = style.lineWidth;
+        c.textAlign = style.textAlign;
+        c.textBaseline = style.textBaseline;
+    }
+    applyBkgStyle(c) {
+
+        // set style based on clicked
+        let style = this.clicked ? this.clickedStyle : this.unClickedStyle;
+
+        c.fillStyle = style.bkgColor;
+        c.strokeStyle = style.bkgBorderColor;
+        c.lineWidth = style.bkgBorderWidth;
+    }
+}
+
+export class TextElement {
+    constructor({uiParent, loc, width, height, style, visible}) {
+        this.uiParent = uiParent;
+        this.loc = loc;
+        this.width = width;
+        this.height = height;
+        this.style = style;
+
+        // set visibility
+        if (visible === undefined) {
+            this.visible = true;
+        } else {
+            this.visible = visible;
+        }
+    }
+    draw(c) {
+
+        // if not visible return early
+        if (!this.visible) {return;}
+
+
+        c.save();
+
+        // apply uiParent loc
+        if (this.uiParent !== undefined) {
+            c.translate(this.uiParent.loc.x, this.uiParent.loc.y);
+        }
+
+        // draw element
+        this.applyStyle(c);
         c.fillStyle = 'black';
         c.textAlign = 'center';
         c.textBaseline = 'middle';
         c.fillText(this.text, this.loc.x + this.width/2, this.loc.y + this.height/2);
 
-        c.restore
+        c.restore();
+    }
+    applyStyle(c) {
+        return;
+    }
+}
+
+export class ImageElement {
+    constructor({uiParent, loc, width, height, img, visible}) {
+        this.uiParent = uiParent;
+        this.loc = loc;
+        this.width = width;
+        this.height = height;
+        this.img = img;
+
+        // set visibility
+        if (visible === undefined) {
+            this.visible = true;
+        } else {
+            this.visible = visible;
+        }
+    }
+    draw(c) {
+
+        // if not visible return early
+        if (!this.visible) {return;}
+
+
+        c.save();
+
+        // apply uiParent loc
+        if (this.uiParent !== undefined) {
+            c.translate(this.uiParent.loc.x, this.uiParent.loc.y);
+        }
+
+        // draw element
+        c.drawImage(this.img, this.loc.x, this.loc.y, this.width, this.height);
+
+        c.restore();
     }
 }

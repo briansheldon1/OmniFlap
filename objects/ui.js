a2a3eabe703ac4f1990b1e60780efec9c14425e9
Brian Sheldon <briansouthpas@gmail.com>
2024-08-25 12:58:10 -0700

    swapped ordering of stroke and fill for text

--- objects/ui.js
+++ objects/ui.js
@@ -210,13 +210,16 @@ export class TextElement {
         // draw text
         if (this.textStyle !== undefined) {
             this.applyStyle(c, this.textStyle);
-            c.fillText(this.text, this.loc.x + this.width/2, this.loc.y + this.height/2);
 
             // draw stroke text if lineWidth is defined and non-zero
             if (this.textStyle.lineWidth !== undefined && 
                 this.textStyle.lineWidth !==0) {
                 c.strokeText(this.text, this.loc.x + this.width/2, this.loc.y + this.height/2);
             }
+            
+            c.fillText(this.text, this.loc.x + this.width/2, this.loc.y + this.height/2);
+
+            
         }
 
         c.restore();
